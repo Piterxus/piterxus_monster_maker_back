2751a7f08090e55cf5b39ea9095c18670b1012a0
Piterxus <piterxus@gmail.com>
2024-12-10 11:59:44 +0100

    first commit

---
+++ src/api/shopper/controllers/shopper.ts
@@ -0,0 +1,7 @@
+/**
+ * shopper controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::shopper.shopper');
---
+++ src/api/shopper/routes/shopper.ts
@@ -0,0 +1,7 @@
+/**
+ * shopper router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::shopper.shopper');
