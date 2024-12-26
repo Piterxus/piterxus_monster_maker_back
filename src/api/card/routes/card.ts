fc84757c2e58034fa84c9a2935d49a5e767c2f76
piterxus <piterxus@gmail.com>
2024-12-26 20:46:12 +0100

    test: Testing reservation endpoint

--- src/api/card/routes/card.ts
+++ src/api/card/routes/card.ts
@@ -1,31 +1,43 @@
-// src/api/card/routes/card.ts
+/**
+ * card router
+ */
 
 import { factories } from '@strapi/strapi';
 
-// Define las rutas predeterminadas (find, update) de Strapi
-export default factories.createCoreRouter('api::card.card', {
-  only: ['find', 'update'], // Solo las rutas predeterminadas 'find' y 'update'
-  config: {
-    find: {
-      auth: false,
-    },
-    update: {
-      auth: false,
-    },
-  },
-});
+export default factories.createCoreRouter('api::card.card');
 
-// Definir las rutas personalizadas
-export const customRoutes = [
-  {
-    method: 'PUT',
-    path: '/cards/:id/reserve', // Ruta personalizada
-    handler: 'api::card.card.reserve', // Acción del controlador
-    config: {
-      auth: false,
-    },
-  },
-];
 
-// Registra las rutas personalizadas en la configuración del router
-export const routes = [...customRoutes]; // Unifica las rutas predeterminadas y personalizadas
+
+
+
+// // src/api/card/routes/card.ts
+
+// import { factories } from '@strapi/strapi';
+
+// // Define las rutas predeterminadas (find, update) de Strapi
+// export default factories.createCoreRouter('api::card.card', {
+//   only: ['find', 'findOne', 'update'], // Solo las rutas predeterminadas 'find' y 'update'
+//   config: {
+//     find: {
+//       auth: false,
+//     },
+//     update: {
+//       auth: false,
+//     },
+//   },
+// });
+
+// // Definir las rutas personalizadas
+// export const customRoutes = [
+//   {
+//     method: 'PUT',
+//     path: '/cards/:id/reserve', // Ruta personalizada
+//     handler: 'api::card.card.reserve', // Acción del controlador
+//     config: {
+//       auth: false,
+//     },
+//   },
+// ];
+
+// // Registra las rutas personalizadas en la configuración del router
+// export const routes = [...customRoutes]; // Unifica las rutas predeterminadas y personalizadas
