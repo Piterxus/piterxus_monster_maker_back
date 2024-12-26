a747bc7224202ec4d2de6e8f2131c30d1892fda6
piterxus <piterxus@gmail.com>
2024-12-26 17:54:35 +0100

    test: Testing reservation endpoint

--- src/api/card/routes/card.ts
+++ src/api/card/routes/card.ts
@@ -1,7 +1,31 @@
-/**
- * card router
- */
+// src/api/card/routes/card.ts
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::card.card');
+// Define las rutas predeterminadas (find, update) de Strapi
+export default factories.createCoreRouter('api::card.card', {
+  only: ['find', 'update'], // Solo las rutas predeterminadas 'find' y 'update'
+  config: {
+    find: {
+      auth: false,
+    },
+    update: {
+      auth: false,
+    },
+  },
+});
+
+// Definir las rutas personalizadas
+export const customRoutes = [
+  {
+    method: 'PUT',
+    path: '/cards/:id/reserve', // Ruta personalizada
+    handler: 'api::card.card.reserve', // Acción del controlador
+    config: {
+      auth: false,
+    },
+  },
+];
+
+// Registra las rutas personalizadas en la configuración del router
+export const routes = [...customRoutes]; // Unifica las rutas predeterminadas y personalizadas
